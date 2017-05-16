ef9684b3c3dee4009b0465d87417bc9c44d4097d
tiwariHD <tiwari@stud.uni-heidelberg.de>
2017-05-16 17:23:54 +0200

    name change to goDrmCdi

--- examples/README.md
+++ examples/README.md
@@ -18,7 +18,7 @@
     }
 }
 
-#env CDI_COMMAND=VERSION ./goDrm < drm.conf
+#env CDI_COMMAND=VERSION ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.2",
     "supportedVersions": [
@@ -27,7 +27,7 @@
     ]
 }
 
-#env CDI_COMMAND=INFO ./goDrm < drm.conf
+#env CDI_COMMAND=INFO ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.2",
     "gpu": 2,
@@ -37,7 +37,7 @@
     ]
 }
 
-#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.1",
     "devices": [
@@ -46,7 +46,7 @@
     ]
 }
 
-#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:2 CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:2 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.1",
     "code": 102,
@@ -54,12 +54,12 @@
     "details": "No of GPU available: 0"
 }
 
-#env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+#env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.1"
 }
 
-#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1,gpu-memory=2048Mi CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1,gpu-memory=2048Mi CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.1",
     "code": 4,
@@ -67,14 +67,14 @@
     "details": "Unsupported resource request: gpu:1,gpu-memory=2048Mi"
 }
 
-#env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=3456 ./goDrm < drm.conf
+#env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=3456 ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.1",
     "code": 5,
     "msg": "Unknown container ID"
 }
 
-#env CDI_VERSION=0.999 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+#env CDI_VERSION=0.999 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.2",
     "code": 1,
@@ -82,14 +82,14 @@
     "details": "Unsupported version: 0.999"
 }
 
-#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 ./goDrm < drm.conf
+#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 ./goDrmCdi < drm.conf
 {
     "cdiVersion": "0.0.1",
     "code": 103,
     "msg": "Container ID not specified"
 }
 
-#env CDI_VERSION=0.0.1 CDI_COMMAND=MYCMD ./goDrm < drm.conf
+#env CDI_VERSION=0.0.1 CDI_COMMAND=MYCMD ./goDrmCdi < drm.conf
 {
     "cdiVersion": "",
     "code": 3,
@@ -97,7 +97,7 @@
     "details": "Unsupported command MYCMD"
 }
 
-#env CDI_VERSION=0.0.1 CDI_REQUEST=gpu:1 ./goDrm < drm.conf
+#env CDI_VERSION=0.0.1 CDI_REQUEST=gpu:1 ./goDrmCdi < drm.conf
 {
     "cdiVersion": "",
     "code": 100,
--- examples/examples.sh
+++ examples/examples.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 #for static builds
-go build -ldflags '-extldflags "-static -lm"' ../goDrm.go
+go build -ldflags '-extldflags "-static -lm"' ../goDrmCdi.go
 #rm -r ../drmFiles
 
 echo "#cat drm.conf"
 cat drm.conf
 
-echo "#env CDI_COMMAND=VERSION ./goDrm < drm.conf" 
-env CDI_COMMAND=VERSION ./goDrm < drm.conf 
+echo "#env CDI_COMMAND=VERSION ./goDrmCdi < drm.conf" 
+env CDI_COMMAND=VERSION ./goDrmCdi < drm.conf 
 
-echo "#env CDI_COMMAND=INFO ./goDrm < drm.conf" 
-env CDI_COMMAND=INFO ./goDrm < drm.conf 
+echo "#env CDI_COMMAND=INFO ./goDrmCdi < drm.conf" 
+env CDI_COMMAND=INFO ./goDrmCdi < drm.conf 
 
-echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrm < drm.conf"
-env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 
-echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:2 CDI_REQUEST_ID=1234 ./goDrm < drm.conf"
-env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:2 CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:2 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:2 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 
-echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=1234 ./goDrm < drm.conf"
-env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 
-echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1,gpu-memory=2048Mi CDI_REQUEST_ID=1234 ./goDrm < drm.conf"
-env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1,gpu-memory=2048Mi CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1,gpu-memory=2048Mi CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1,gpu-memory=2048Mi CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 
-echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=3456 ./goDrm < drm.conf"
-env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=3456 ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=3456 ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.0.1 CDI_COMMAND=DEL CDI_REQUEST_ID=3456 ./goDrmCdi < drm.conf
 
-echo "#env CDI_VERSION=0.999 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrm < drm.conf"
-env CDI_VERSION=0.999 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.999 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.999 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 CDI_REQUEST_ID=1234 ./goDrmCdi < drm.conf
 
-echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 ./goDrm < drm.conf"
-env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.0.1 CDI_COMMAND=ADD CDI_REQUEST=gpu:1 ./goDrmCdi < drm.conf
 
-echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=MYCMD ./goDrm < drm.conf"
-env CDI_VERSION=0.0.1 CDI_COMMAND=MYCMD ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.0.1 CDI_COMMAND=MYCMD ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.0.1 CDI_COMMAND=MYCMD ./goDrmCdi < drm.conf
 
-echo "#env CDI_VERSION=0.0.1 CDI_REQUEST=gpu:1 ./goDrm < drm.conf"
-env CDI_VERSION=0.0.1 CDI_REQUEST=gpu:1 ./goDrm < drm.conf
+echo "#env CDI_VERSION=0.0.1 CDI_REQUEST=gpu:1 ./goDrmCdi < drm.conf"
+env CDI_VERSION=0.0.1 CDI_REQUEST=gpu:1 ./goDrmCdi < drm.conf
 
